139e6e28664bfc2c07c0949b725c4203e5f4ec30
fadhil045 <ahmad.123490045@student.itera.ac.id>
2025-03-08 19:50:32 +0700

    Add files via upload

---
+++ java.script.js
@@ -0,0 +1,3 @@
+function tampilkanPesan() {
+    alert("Haiii!!, selamat atas kelulusan kamu, semoga cepet dapet kerja yang layak yaa, kerjanya serius gajinya juga serius.semoga ini kamu dapatkan pada 2 tahun kedepan!");
+}
